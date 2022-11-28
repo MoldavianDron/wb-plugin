b8e2f5ebb15d7e2c57d6d5a1e757d9e9de7e5c26
azaporojan <a.zaporojan@appfire.com>
2022-11-28 13:42:23 +0200

    static weather plugin

--- plugin/src/PluginRoot.js
+++ plugin/src/PluginRoot.js
@@ -1,27 +1,18 @@
 import {useEffect, useState} from "react";
+import moment from 'moment';
 import { registerTemplate } from "@whiteboards-io/plugins";
-import weatherImage from "./weather-mops.jpeg";
+import weatherImage from "./weather.jpeg";
+
+const data = {
+  name: "Warszawa",
+  description: "Rainy",
+  temperature: "25",
+  humidity: "67",
+  sunrise: 1646306882,
+  sunset: 1646347929
+}
 
 export default function PluginRoot() {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
-  const [data, setData] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
-      
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-          setData(result)
-        });
-    }
-    fetchData();
-  }, [lat,long]);
   
   useEffect(() => {
     registerTemplate({
@@ -34,14 +25,90 @@ export default function PluginRoot() {
         cards: {
           _index: {
             cardLocation: true,
+            date: true,
+            description: true,
+            temperature: true,
+            humidity: true,
+            sunrise: true,
+            sunset: true,
           },
           _items: {
             cardLocation: {
               x: 0,
               y: 0,
-              width: 250,
-              height: 40,
-              text: `${typeof data.main != 'undefined' ? `${data.name}` : "Waiting for response"}`,
+              width: 1000,
+              height: 50,
+              color: "#AAAAAA",
+              textColor: "#ffffff",
+              fontSize: 40,
+              textAlign: 1,
+              text: `${data.name}`,
+            },
+            date: {
+              x: 0,
+              y: 50,
+              width: 500,
+              height: 50,
+              color: "#A3D6FB",
+              textColor: "#ffffff",
+              fontSize: "30",
+              textAlign: 1,
+              text: `${moment().format('dddd')}, ${moment().format('LL')}`,
+            },
+            description: {
+              x: 500,
+              y: 50,
+              width: 500,
+              height: 50,
+              color: "#A3D6FB",
+              textColor: "#ffffff",
+              fontSize: "30",
+              textAlign: 2,
+              text: `${data.description}`,
+            },
+            temperature: {
+              x: 0,
+              y: 100,
+              width: 500,
+              height: 50,
+              color: "#A3D6FB",
+              textColor: "#ffffff",
+              fontSize: "20",
+              textAlign: 1,
+              text: `Temperature: ${data.temperature} C`,
+            },
+            humidity: {
+              x: 500,
+              y: 100,
+              width: 500,
+              height: 50,
+              color: "#A3D6FB",
+              textColor: "#ffffff",
+              fontSize: "20",
+              textAlign: 2,
+              text: `Humidity: ${data.humidity} %`,
+            },
+            sunrise: {
+              x: 0,
+              y: 150,
+              width: 500,
+              height: 50,
+              color: "#A3D6FB",
+              textColor: "#ffffff",
+              fontSize: "20",
+              textAlign: 1,
+              text: `Sunrise: ${new Date(data.sunrise * 1000).toLocaleTimeString('en-IN')}`,
+            },
+            sunset: {
+              x: 500,
+              y: 150,
+              width: 500,
+              height: 50,
+              color: "#A3D6FB",
+              textColor: "#ffffff",
+              fontSize: "20",
+              textAlign: 2,
+              text: `Sunset: ${new Date(data.sunset * 1000).toLocaleTimeString('en-IN')}`,
             },
           },
         },
